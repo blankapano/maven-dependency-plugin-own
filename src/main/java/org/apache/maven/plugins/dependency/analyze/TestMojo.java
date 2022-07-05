dc9a03decdcadb07939a888bdd6656751b59d789
idiotopano <tingyx96@gmail.com>
2022-07-05 15:35:20 +0800

    test
    test

---
+++ src/main/java/org/apache/maven/plugins/dependency/analyze/TestMojo.java
@@ -0,0 +1,16 @@
+package org.apache.maven.plugins.dependency.analyze;
+
+import junit.framework.TestCase;
+
+public class TestMoJo
+    extends TestCase
+{
+
+    AnalyzeDepMgt mojo;
+
+    protected void setUp()
+        throws Exception
+    {
+        mojo = new AnalyzeDepMgt();
+    }
+}
